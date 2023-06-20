cfdaa3feb1195548dfb31084ffe78d0bd737697f
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-06-20 14:39:38 +0200

    Init progetto Start pari e dispari

---
+++ Pari e Dispari/js/my-script.js
@@ -0,0 +1,18 @@
+// Pari e Dispari
+// L'utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+// .1 prendiamo un dato dall' utente tra pari e dispari e un numero tra 1 e 5
+
+// .2 scrivo una funzione che generi un numero da 1 a 5 per il cpu
+
+// .3 sommiamo i due numeri 
+
+// .4 stabiliamo se la somma è pari o dispari, con una funzione
+
+// .5 stampo il vincitore
+
+
